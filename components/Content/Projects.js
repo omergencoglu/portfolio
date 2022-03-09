d9a38990e597f2360fd169b65856344bf23eb9b3
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-03-09 16:33:12 -0500

    Start creating projects section

--- components/Content/Projects.js
+++ components/Content/Projects.js
@@ -1,9 +1,14 @@
+import Card from "../UI/Card";
+
 import styles from "./Projects.module.css";
 
 function Projects() {
   return (
     <div className={styles.projects}>
       <h2 id="projects">PROJECTS</h2>
+      <div className={styles.cards_wrapper}>
+        <Card img="/placeholder.jpg">Hiiii</Card>
+      </div>
     </div>
   );
 }
--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 import styles from "./Card.module.css";
 
 const Card = (props) => {
   const classes = `${styles.card} ${props.className}`;
-  return <div className={classes}>{props.children}</div>;
+  return (
+    <div className={classes}>
+      <Image src={props.img} alt={props.alt} width={500} height={500} />
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default Card;
