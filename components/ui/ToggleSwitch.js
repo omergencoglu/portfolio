10d7facfd4614e5fc7a15134712bb792b43631bd
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-03-18 13:47:07 -0400

    Add ThemeProvider and enable darkmode

--- components/ui/ToggleSwitch.js
+++ components/ui/ToggleSwitch.js
@@ -1,14 +1,31 @@
-import { Fragment } from "react/cjs/react.development";
+import { useState, useEffect, Fragment } from "react";
+import { useTheme } from "next-themes";
+
 import MoonIcon from "./Icons/MoonIcon";
 import SunIcon from "./Icons/SunIcon";
 
 import styles from "./ToggleSwitch.module.css";
 
 function ToggleSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  const themeChanger = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
     <Fragment>
       <label className={styles.switch}>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          onChange={themeChanger}
+          checked={theme === "dark" ? "checked" : ""}
+        />
         <span className={`${styles.slider} ${styles.round}`}>
           <MoonIcon />
           <SunIcon />
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
+import { ThemeProvider } from "next-themes";
+
 import Layout from "../components/layout/Layout";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider defaultTheme="system">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
