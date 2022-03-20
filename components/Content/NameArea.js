baeb6b4e1452268b33f5c31ecf9e7f2e993dbad6
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-03-20 19:37:40 -0400

    NameArea style update

--- components/Content/NameArea.js
+++ components/Content/NameArea.js
@@ -11,7 +11,6 @@ function NameArea() {
           alt="Omer Gencoglu"
           width={100}
           height={100}
-          className={styles.image}
           layout="responsive"
         />
       </div>
