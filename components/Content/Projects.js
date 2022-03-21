8818f40f442a9fdf0304c495e497f755d8540ef3
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-03-21 16:31:31 -0400

    Update cards

--- components/Content/Projects.js
+++ components/Content/Projects.js
@@ -16,36 +16,39 @@ function Projects() {
           eligendi porro officia dignissimos natus?
         </Card>
         <Card
-          img="/weather-app.png"
-          keywords="React Next.js"
-          title="Test Project"
+          img="/yelpcamp.png"
+          keywords="Node.js Express MongoDB"
+          title="YelpCamp"
+          previewLink="https://omersyelpcamp.herokuapp.com/"
+          githubLink="https://github.com/omergencoglu/yelpcamp"
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          eligendi porro officia dignissimos natus?
+          Fully functional NodeJs web app - Allows users to view, post, comment
+          on campgrounds worldwide.
         </Card>
         <Card
-          img="/weather-app.png"
-          keywords="React Next.js"
-          title="Test Project"
+          img="/google-dark.png"
+          keywords="HTML CSS"
+          title="Google Homepage"
+          previewLink="https://omergencoglu.github.io/Google-Homepage-Clone-Darkmode/"
+          githubLink="https://github.com/omergencoglu/Google-Homepage-Clone-Darkmode"
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          eligendi porro officia dignissimos natus?
+          Google homepage dark mode clone - to practice HTML/CSS.
         </Card>
         <Card
-          img="/weather-app.png"
-          keywords="React Next.js"
-          title="Test Project"
+          img="/food-order.png"
+          keywords="React Firebase"
+          title="Food Ordering App"
+          githubLink="https://github.com/omergencoglu/food-order-app-react"
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          eligendi porro officia dignissimos natus?
+          You can add items to your cart and cart will update automatically.
         </Card>
         <Card
-          img="/weather-app.png"
-          keywords="React Next.js"
-          title="Test Project"
+          img="/user-management.png"
+          keywords="Node.js Express MongoDB"
+          title="User Management"
+          githubLink="https://github.com/omergencoglu/user-management-system-crud"
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          eligendi porro officia dignissimos natus?
+          CRUD application with Express and MongoDB.
         </Card>
         <Card
           img="/weather-app.png"
--- components/ui/Card.js
+++ components/ui/Card.js
@@ -6,33 +6,39 @@ import ExternalLink from "./Icons/ExternalLink";
 import styles from "./Card.module.css";
 
 const Card = (props) => {
-  const classes = `${styles.card} ${props.className}`;
   return (
-    <div className={classes} data-aos="fade-up">
-      <div>
-        <a href={props.previewLink} target="_blank" rel="noopener noreferrer">
-          <div className={styles.imageContainer}>
-            <Image
-              src={props.img}
-              alt={props.alt}
-              width={1920}
-              height={1080}
-              layout="responsive"
-            />
-          </div>
-          <h3>{props.title}</h3>
-        </a>
-        <p>{props.children}</p>
-      </div>
+    <div className={styles.card} data-aos="fade-up">
+      <a
+        href={props.previewLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.cardHeader}
+      >
+        <Image
+          src={props.img}
+          alt={props.title}
+          width={1080}
+          height={608}
+          layout="responsive"
+        />
+        <h3>{props.title}</h3>
+      </a>
+      <p className={styles.cardMain}>{props.children}</p>
       <div className={styles.cardFooter}>
         <span>{props.keywords}</span>
         <div className={styles.links}>
           <a href={props.githubLink} target="_blank" rel="noopener noreferrer">
             <Github />
           </a>
-          <a href={props.previewLink} target="_blank" rel="noopener noreferrer">
-            <ExternalLink />
-          </a>
+          {props.previewLink && (
+            <a
+              href={props.previewLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ExternalLink />
+            </a>
+          )}
         </div>
       </div>
     </div>
